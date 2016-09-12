1f8fb5d71bc4bd3038c321bfab94960f37e59603
Andrey <a.stegno@gmail.com>
2016-09-12 21:16:57 +0000

    Added compiler itself

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-console.log("In Development")
+var exports = require("./compiler.js")
+
+var compile = function(code) {
+      var result = exports.compile(code)
+      var code = JSON.parse(result).js_code
+      if(typeof code !== "undefined") {
+        var lines = code.split("\n")
+        lines.shift()
+       return lines.join("\n")
+      }
+      return result
+}
+
+module.exports = {
+
+   compile: compile
+
+}
